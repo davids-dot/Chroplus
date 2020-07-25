584d2b9d408e48c80b39156c5b5fcf9bf6bdc4a9
zhaoyanchao <zhaoyanchao@secoo.com>
2020-07-25 08:28:12 +0800

    上传代码

---
+++ chroplus.js
@@ -0,0 +1,21 @@
+window.onload = function(){
+    if(window.location.href.indexOf('http://blog.sina.com.cn/u/6274555375') != -1) {
+        setInterval(refreshSina,5000);
+    } else if (window.location.href.indexOf('www.baidu.com')!= -1) {
+        setInterval(clearBaiDuAdvertisement,50);
+    }
+};
+
+
+function refreshSina() {
+  window.location.reload();
+}
+
+function clearBaiDuAdvertisement() {
+    var h = document.getElementsByClassName('cr-content');
+    if (h.length > 0) {
+        for(let i =0; i< h.length; i++ ) {
+            h[i]['style']['display'] = 'none';
+        }
+    }
+}
