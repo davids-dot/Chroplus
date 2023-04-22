7a1c37c9df89063dade41e9ebd12470db5c95184
zhaoyanchao <yanchao.zhao@vdx.com>
2023-04-22 22:52:02 +0800

    屏蔽掉百度个人主页的推荐

--- baidu.js
+++ baidu.js
@@ -6,9 +6,14 @@ window.onload = function(){
     console.log('插件进入')
 
     if (window.location.href.indexOf('www.baidu.com')!= -1) {
-        setInterval(clearBaiDuAdvertisement,100);
+        setInterval(setBaiduConfig,100);
     }
 };
+
+function setBaiduConfig() {
+    clearBaiDuAdvertisement()
+    setDefaultFoucusOnMyMenu()
+}
 
 function clearBaiDuAdvertisement() {
     var h = document.getElementsByClassName('cr-content');
@@ -19,6 +24,16 @@ function clearBaiDuAdvertisement() {
         console.log("loop begin")
         for(let i =0; i< h.length; i++ ) {
             h[i]['style']['display'] = 'none';
+        }
+    }
+}
+
+function setDefaultFoucusOnMyMenu() {
+    let mine = document.getElementById('s_menu_mine')
+    if (mine) {
+        let classNames = mine.getAttribute('class')
+        if (classNames.indexOf('current') == -1) {
+            mine.click()
         }
     }
 }
